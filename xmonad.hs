85245f0c63683bf43a8fa65ac55d496090071d66
Li Zhai <zhaili@chanct.com>
2016-04-22 20:53:40 +0800

    add some shortcuts

--- xmonad.hs
+++ xmonad.hs
@@ -1,18 +1,21 @@
 import XMonad
 import XMonad.Layout.NoBorders
 
---import XMonad.Util.Run(spawnPipe)
---import System.IO
+import XMonad.Util.Run(spawnPipe)
+import System.IO
+import qualified Data.Map as M
 
---import qualified Data.Map as M
-
-
---myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
---  [ ((modMask, xK_l), spawn "xscreensaver-command -lock; xset dpms force off")
---        -- other keybindings here
---      ]
 
 main = xmonad defaultConfig
     { terminal    = "urxvt"
     , modMask     = mod4Mask
+    ,keys         = myKeys                    
     }
+    
+myKeys x = M.union (M.fromList (newKeys x)) (keys defaultConfig x)    
+
+newKeys conf@(XConfig {XMonad.modMask = modm}) = [
+  -- ((modm, xK_l), spawn "xscreensaver-command -lock")
+  ((modm .|. shiftMask, xK_l), spawn "xtrlock")
+  -- other keybindings here
+  ]
