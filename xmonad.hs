f7194ef65c0d3d343f41782d7ce0088d2ad4772c
Li Zhai <zhaili@chanct.com>
2016-04-23 09:18:01 +0800

    use slock replace xtrlock

--- xmonad.hs
+++ xmonad.hs
@@ -16,6 +16,7 @@ myKeys x = M.union (M.fromList (newKeys x)) (keys defaultConfig x)
 
 newKeys conf@(XConfig {XMonad.modMask = modm}) = [
   -- ((modm, xK_l), spawn "xscreensaver-command -lock")
-  ((modm .|. shiftMask, xK_l), spawn "xtrlock")
+  -- ((modm .|. shiftMask, xK_l), spawn "xtrlock")
+  ((modm .|. shiftMask, xK_l), spawn "slock")
   -- other keybindings here
   ]
