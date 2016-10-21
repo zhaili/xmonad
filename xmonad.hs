e408b2d9bb59036ad65a280f24be8c0f07bced5c
Li Zhai <zhaili@chanct.com>
2016-10-21 13:55:01 +0800

    move specified window to a given workspace

--- xmonad.hs
+++ xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.InsertPosition
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageHelpers
 
 import XMonad.Util.Run(spawnPipe)
 import System.IO
@@ -17,16 +18,25 @@ takeScreenshot = do
   let iso = formatTime defaultTimeLocale "%FT%T" t
   spawn $ "import 'screenshot_" ++ iso ++ ".png'"
 
+myManageHook = composeOne
+    [
+      className =? "Emacs"             -?> doShift "1:dev"
+    , className =? "Google-chrome"     -?> doShift "2:web"
+    , className =? "Icedove"           -?> doShift "5:mail"
+    , isDialog                         -?> doFloat
+    ]
+
 main = xmonad $ ewmh defaultConfig
     { terminal    = "urxvt"
     , modMask     = mod4Mask
-    ,keys         = myKeys
-    ,manageHook   = insertPosition End Newer
-    ,handleEventHook = handleEventHook
+    , keys        = myKeys
+    , workspaces  = ["1:dev","2:web","3","4","5:mail","6","7","8","9","0","-","="]
+    , manageHook  = myManageHook <+> insertPosition End Newer
+    , handleEventHook = handleEventHook
                        defaultConfig <+> fullscreenEventHook
     }
-    
-myKeys x = M.union (M.fromList (newKeys x)) (keys defaultConfig x)    
+
+myKeys x = M.union (M.fromList (newKeys x)) (keys defaultConfig x)
 
 newKeys conf@(XConfig {XMonad.modMask = modm}) = [
   -- ((modm, xK_l), spawn "xscreensaver-command -lock")
